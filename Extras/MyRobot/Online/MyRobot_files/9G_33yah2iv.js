35d3ccb60b8dd99e7f14ad4bb4eb26418d7c7873
kevin <kevin.riedl.privat@gmail.com>
2018-07-18 00:30:39 +0200

    remove html files

--- Extras/MyRobot/Online/MyRobot_files/9G_33yah2iv.js
+++
@@ -1,5 +0,0 @@
-/*!CK:463487492!*//*1405305528,*/
-
-if (self.CavalryLogger) { CavalryLogger.start_js(["wnNQ0"]); }
-
-__d("XBroadcastRequestSearchSourceControllerURIBuilder",["XControllerURIBuilder"],function(a,b,c,d,e,f){e.exports=b('XControllerURIBuilder').create("\/profile\/broadcast_request\/search_source\/",{existing_ids:{type:"IntVector"},source_obj_id:{type:"String",required:true},value:{type:"String"}});},null);
--- Extras/MyRobot/Online/MyRobot_files/KYig9nbNgz0.js
+++
@@ -1,5 +0,0 @@
-/*!CK:543169056!*//*1406238962,*/
-
-if (self.CavalryLogger) { CavalryLogger.start_js(["9wadr"]); }
-
-__d("KappaWrapper",["AsyncSignal","setTimeoutAcrossTransitions"],function(a,b,c,d,e,f,g,h){e.exports={start:function(i,j,k){var l=0,m=function(){new g('/si/kappa/',{Ko:"a"}).send();if(++l<i)h(m,(j*1000));};h(m,((j+k)*1000));}};},null);
